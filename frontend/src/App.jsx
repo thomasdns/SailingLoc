4fd5c7cb7d5ec8c079e374199d7918638cdbefcc
iEdwin10 <pokyyy00@gmail.com>
2025-07-15 14:47:19 +0200

    Fix "Bateaux"
    Fix the "Bateaux" in the footer Home Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Bateaux from "./pages/Bateaux";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/bateaux" element={<Bateaux />} />
       </Routes>
     </BrowserRouter>
   );
